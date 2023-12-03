063cc5dfee793faada3ce81289571a0d2800de90
Halil Ozel <halilozel1903@gmail.com>
2023-12-03 16:15:20 +0300

    English language settings added.

--- WeatherApp/WeatherTableViewController.swift
+++ WeatherApp/WeatherTableViewController.swift
@@ -24,7 +24,7 @@ class WeatherTableViewController: UITableViewController {
         getTodayResult()
         
         // yenileme işlemleri için gerekli ayarlamalar
-        refreshAction.addTarget(self, action: #selector(refreshNow), for: UIControlEvents.valueChanged)
+        refreshAction.addTarget(self, action: #selector(refreshNow), for: UIControl.Event.valueChanged)
         
         // renk ayarı
         refreshAction.tintColor = UIColor.red
@@ -54,7 +54,7 @@ class WeatherTableViewController: UITableViewController {
         
         if indexPath.row == 0 {
             let cell = tableView.dequeueReusableCell(withIdentifier: "WeatherCell", for: indexPath) as! WeatherTableViewCell
-            cell.selectedText?.text = "Seçtiğiniz şehir :  \(cityName)"
+            cell.selectedText?.text = "Selected City :  \(cityName)"
             return cell
         }else{
             
@@ -96,41 +96,22 @@ class WeatherTableViewController: UITableViewController {
                         
                         do{
                             let jsonResult =  try JSONSerialization.jsonObject(with: incommingData, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
-                            //  print(jsonResult)
-                            
-                            
-                            //let weather = jsonResult["weather"] as! NSArray
-                            
-                            //let weather1 = weather.firstObject as! [String : AnyObject]
-                            
+                        
                             
                             if let main = jsonResult["main"] as? NSDictionary{
-                                // print("Seçtiğin şehrin hava durum bilgisi : \(description)")
+                            
                                 
                                 if let temp = main["temp"] as? Double{
                                     
                                     let state = (Int)(temp - 273.15)
-                                    
-                                    //print(description)
-                                    
-                                    
+                    
                                     DispatchQueue.main.sync(execute: {
                                         self.currentWeather = String (state)
                                         self.tableView.reloadData()
                                     })
                                 }
                             }
-                            
-                            //    let degrees = jsonResult["main"] as! NSObject
-                            
-                            //  let degress1 = degrees.value(forKey: "temp") as! ["String" : Double]
-                            
-                            /* if let desc = degress1["temp"] as? Double{
-                             print("\(desc)")
-                             
-                             
-                             }*/
-        
+                    
                             
                         }catch{
                             print("Hata oluştu")
